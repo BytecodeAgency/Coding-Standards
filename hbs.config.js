ff31acdc9e69f26e2012cdce133773fa82fee989
Luciano Nooijen <lucianonooijen@gmail.com>
2018-05-19 13:58:02 +0200

    Added Handlebars support

---
+++ hbs.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    data: 'src/includes/data',
+    decorators: 'src/includes/decorators',
+    helpers: 'src/includes/helpers',
+    layouts: 'src/includes/layouts',
+    partials: 'src/includes/partials',
+};
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static('dist'));
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
