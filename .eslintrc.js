ee2a2bbb125b2efc25c8057d91a1b65d5f8a01b3
Luciano Nooijen <luciano@bytecode.nl>
2019-07-27 02:06:27 +0200

    Added required ESLint plugins
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
     parser:  '@typescript-eslint/parser',
     extends:  [
         'airbnb',
-        'plugin:react/recommended',
         'plugin:@typescript-eslint/recommended',
         'prettier/@typescript-eslint',
         'plugin:prettier/recommended',
